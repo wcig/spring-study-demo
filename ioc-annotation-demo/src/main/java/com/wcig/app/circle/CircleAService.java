a4b868dda6021043903b2f55c904d4f813ed59a5
wcig <job.yangbo@gmail.com>
2021-03-09 11:46:51 +0800

    Spring解决循环依赖注入方式

---
+++ ioc-annotation-demo/src/main/java/com/wcig/app/circle/CircleAService.java
@@ -0,0 +1,21 @@
+package com.wcig.app.circle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Spring解决循环依赖注入的方式: 先调用构造函数构造对下,然后通过set方法或反射将依赖的对象添加到对象的变量中去.
+ * 验证方式: 只添加有参的构造函数,而省略无参构造函数
+ */
+@Component
+public class CircleAService {
+    @Autowired
+    private CircleBService circleBService;
+
+    public CircleAService() {
+    }
+
+    public CircleAService(CircleBService circleBService) {
+        this.circleBService = circleBService;
+    }
+}
