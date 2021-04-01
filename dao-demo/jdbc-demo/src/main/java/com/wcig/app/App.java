d09cc7a0e0515c7652250f37429173ce37acb597
wcig <job.yangbo@gmail.com>
2021-04-01 22:08:37 +0800

    @Slf4j

--- dao-demo/jdbc-demo/src/main/java/com/wcig/app/App.java
+++ dao-demo/jdbc-demo/src/main/java/com/wcig/app/App.java
@@ -1,14 +1,11 @@
 package com.wcig.app;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App {
-
-    static final Logger logger = LoggerFactory.getLogger(App.class);
-
     public static void main( String[] args ) {
-        logger.info("hello world");
+        log.info("hello world ok");
     }
 }
