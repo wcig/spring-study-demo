d1f018d7a75aaf550f29727a24704c2e85415b8d
wcig <job.yangbo@gmail.com>
2021-04-03 13:17:22 +0800

    spring jdbc

--- dao-demo/jdbc-demo/src/main/java/com/wcig/app/dao/UserDao.java
+++ dao-demo/jdbc-demo/src/main/java/com/wcig/app/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Repository
 public class UserDao {
@@ -22,5 +23,22 @@ public class UserDao {
         String sql = "SELECT * FROM user WHERE id = ?";
         RowMapper<User> mapper = new BeanPropertyRowMapper<>(User.class);
         return jdbcTemplate.queryForObject(sql, mapper, id);
+    }
+
+    public List<User> selectByName(String name) {
+        String sql = "SELECT * FROM user WHERE name = ?";
+        RowMapper<User> mapper = new BeanPropertyRowMapper<>(User.class);
+        return jdbcTemplate.query(sql, mapper, name);
+    }
+
+    public boolean updatePasswordById(long id, String password) {
+        String sql = "UPDATE user SET password = ? WHERE id = ?";
+        RowMapper<User> mapper = new BeanPropertyRowMapper<>(User.class);
+        return jdbcTemplate.update(sql, password, id) > 0;
+    }
+
+    public boolean deleteById(long id) {
+        String sql = "DELETE FROM user WHERE id = ?";
+        return jdbcTemplate.update(sql, id) > 0;
     }
 }
--- dao-demo/jdbc-demo/src/test/java/com/wcig/app/dao/UserDaoTest.java
+++ dao-demo/jdbc-demo/src/test/java/com/wcig/app/dao/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,20 +21,61 @@ public class UserDaoTest {
 
     @Test
     public void testInsert() {
-        User user = new User();
-        user.setName("jerry");
-        user.setPhone("13500002222");
-        user.setPassword("123456");
-        user.setCreateTime(System.currentTimeMillis());
-        boolean result = userDao.insert(user);
-        log.info("insert user result: {}", result);
+        User u1 = new User();
+        u1.setName("tom");
+        u1.setPhone("13500001111");
+        u1.setPassword("123456");
+        u1.setCreateTime(System.currentTimeMillis());
+        boolean result = userDao.insert(u1);
+        log.info("insert user1 result: {}", result);
+
+        User u2 = new User();
+        u2.setName("jerry");
+        u2.setPhone("13500002222");
+        u2.setPassword("123456");
+        u2.setCreateTime(System.currentTimeMillis());
+        result = userDao.insert(u2);
+        log.info("insert user2 result: {}", result);
     }
 
     @Test
     public void testSelectById() {
-        long id = 1L;
-        User user = userDao.selectById(id);
-        log.info("select by id, user: {}", user);
+        String name = "tom";
+        List<User> list = userDao.selectByName(name);
+        if (!list.isEmpty()) {
+            long id = list.get(0).getId();
+            User user = userDao.selectById(id);
+            log.info("select by id, user: {}", user);
+        }
     }
 
+    @Test
+    public void testSelectByName() {
+        String name = "tom";
+        List<User> list = userDao.selectByName(name);
+        log.info("select by name, user list: {}", list);
+    }
+
+    @Test
+    public void testUpdatePasswordById() {
+        String name = "tom";
+        List<User> list = userDao.selectByName(name);
+        if (!list.isEmpty()) {
+            long id = list.get(0).getId();
+            String newPwd = "111111";
+            boolean result = userDao.updatePasswordById(id, newPwd);
+            log.info("update user result: {}", result);
+        }
+    }
+
+    @Test
+    public void testDeleteById() {
+        String name = "tom";
+        List<User> list = userDao.selectByName(name);
+        if (!list.isEmpty()) {
+            long id = list.get(0).getId();
+            boolean result = userDao.deleteById(id);
+            log.info("delete user result: {}", result);
+        }
+    }
 }
