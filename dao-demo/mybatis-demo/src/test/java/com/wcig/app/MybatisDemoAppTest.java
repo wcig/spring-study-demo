5f9c5d40a0e155ac8c1b287857de21454c22e464
wcig <job.yangbo@gmail.com>
2021-04-05 18:51:34 +0800

    mybatis常用xml方式

--- dao-demo/mybatis-demo/src/test/java/com/wcig/app/MybatisDemoAppTest.java
+++ dao-demo/mybatis-demo/src/test/java/com/wcig/app/MybatisDemoAppTest.java
@@ -2,7 +2,6 @@ package com.wcig.app;
 
 import com.wcig.app.dao.FirstMapper;
 import com.wcig.app.model.User;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,23 +11,21 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 
-@Slf4j
 public class MybatisDemoAppTest {
-
     // 不使用spring从xml文件中构建SqlSessionFactory
     @Test
     public void testMybatisSimple() {
-        String resource = "mybatis-config.xml";
         try {
+            String resource = "mybatis-config.xml";
             InputStream inputStream = Resources.getResourceAsStream(resource);
-            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
             SqlSession session = sqlSessionFactory.openSession();
             FirstMapper firstMapper = session.getMapper(FirstMapper.class);
-            User user = firstMapper.selectById(4);
-            log.info("result user: {}", user);
+            User user = firstMapper.selectById(37);
+            System.out.println("result user: " + user);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
