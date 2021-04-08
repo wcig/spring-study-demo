53adf7ec04a2e2db3385fb10a020e89a51a46ff6
wcig <job.yangbo@gmail.com>
2021-04-08 20:41:50 +0800

    mybatis delete

---
+++ dao-demo/mybatis-demo/src/main/java/com/wcig/app/dao/sample/DeleteMapper.java
@@ -0,0 +1,12 @@
+package com.wcig.app.dao.sample;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface DeleteMapper {
+    int deleteByPk(@Param("id") Long id);
+    int deleteByMap(@Param("condition") Map<String, Object> map);
+    int batchDeleteByPk(@Param("list") List<Long> list);
+}
