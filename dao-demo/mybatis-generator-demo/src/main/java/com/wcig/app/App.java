a58b92bb7d72dd730b83cd151039bb957731836b
wcig <job.yangbo@gmail.com>
2021-04-15 23:57:59 +0800

    mybatis generator

---
+++ dao-demo/mybatis-generator-demo/src/main/java/com/wcig/app/App.java
@@ -0,0 +1,23 @@
+package com.wcig.app;
+
+import org.apache.ibatis.io.Resources;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main( String[] args ) throws Exception {
+        List<String> warnings = new ArrayList<>();
+        InputStream inputStream = Resources.class.getClassLoader().getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(inputStream);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
