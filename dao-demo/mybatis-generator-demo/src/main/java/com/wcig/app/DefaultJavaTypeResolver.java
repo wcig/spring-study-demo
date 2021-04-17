fa31564e8e08c843ad268689106c43246f063270
wcig <job.yangbo@gmail.com>
2021-04-17 21:12:44 +0800

    mybatis generator

---
+++ dao-demo/mybatis-generator-demo/src/main/java/com/wcig/app/DefaultJavaTypeResolver.java
@@ -0,0 +1,16 @@
+package com.wcig.app;
+
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
+
+import java.sql.Types;
+
+public class DefaultJavaTypeResolver extends JavaTypeResolverDefaultImpl {
+    public DefaultJavaTypeResolver() {
+        super();
+        this.typeMap.put(Types.TINYINT, new JdbcTypeInformation("TINYINT",
+                new FullyQualifiedJavaType(Integer.class.getTypeName())));
+        this.typeMap.put(Types.SMALLINT, new JdbcTypeInformation("SMALLINT",
+                new FullyQualifiedJavaType(Integer.class.getTypeName())));
+    }
+}
